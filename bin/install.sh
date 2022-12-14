357057514b619413b2a900a4897399de249ceca0
Toru Asai <trsxxii@gmail.com>
2022-12-14 23:14:11 +0900

    Create install.sh

---
+++ bin/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo 'install xcode'
+xcode-select --install
+
+echo 'install rosetta'
+sudo softwareupdate --install-rosetta --agree-to-licensesudo softwareupdate --install-rosetta --agree-to-license
+
+echo 'install homebrew'
+which /opt/homebrew/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'brew doctor, update, upgrade'
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew doctor && brew update --verbose && brew upgrade --verbose
+
+echo 'install app from brewfile'
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew bundle --file ../.Brewfile --verbose
