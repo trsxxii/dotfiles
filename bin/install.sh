ab0913e2f3176ae2b8a9417b56b87dcea36d8131
Toru Asai <trsxxii@gmail.com>
2022-12-16 22:38:22 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "install command line tools"
 xcode-select --install
+
+echo "git clone"
 git clone https://github.com/trsxxii/dotfiles.git && cd dotfiles
+
+echo "make all"
 make all
