622a5fc0180060b41d80b81655b0e275df206301
Toru Asai <trsxxii@gmail.com>
2022-12-16 22:26:35 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-echo 'install rosetta'
-sudo softwareupdate --install-rosetta --agree-to-licensesudo softwareupdate --install-rosetta --agree-to-license
-
-echo 'install homebrew'
-which /opt/homebrew/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-echo 'brew doctor, update, upgrade'
-which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew doctor && brew update --verbose && brew upgrade --verbose
-
-echo 'install app from brewfile'
-which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew bundle --file ../config/Brewfile --verbose
-
-echo 'bundle install'
-gem install bundler
-bundle install --gemfile ../config/Gemfile
-
-echo 'xcversion update'
-xcversion update
+xcode-select --install
+git clone https://github.com/trsxxii/dotfiles.git && cd dotfiles
+make all
