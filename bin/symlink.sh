17a9767d469b006608da279ee81b1f7f735ae63b
Toru Asai <trsxxii@gmail.com>
2022-12-17 10:32:23 +0900

    Update symlink.sh

--- bin/symlink.sh
+++ bin/symlink.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 dotfiles=(
-../config/.zshrc
-../config/.secret.zshrc
+./config/.zshrc
+./config/.secret.zshrc
 )
 
 echo "symbolic link"
 for file in "${dotfiles[@]}"; do
   ln -svf $file ~/
 done 
-ln -svf ../config/karabiner.json ~/.config/karabiner/
-ln -svf ../config/ignore ~/.config/git/
+ln -svf ./config/karabiner.json ~/.config/karabiner/
+ln -svf ./config/ignore ~/.config/git/
