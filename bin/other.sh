9c5658f2241c058be24c8096758e2dc10775d72b
Toru Asai <trsxxii@gmail.com>
2022-12-17 18:27:59 +0900

    Update other.sh

--- bin/other.sh
+++ bin/other.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo 'install rosetta'
-sudo softwareupdate --install-rosetta --agree-to-licensesudo softwareupdate --install-rosetta --agree-to-license
+if [[ "$(uname -m)" == arm64 ]]; then
+  echo 'install rosetta'
+  sudo softwareupdate --install-rosetta --agree-to-licensesudo softwareupdate --install-rosetta --agree-to-license
+fi
