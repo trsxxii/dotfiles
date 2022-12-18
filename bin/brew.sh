696e56838dc1b1c19e52a786a96b4c2048380c84
Toru Asai <trsxxii@gmail.com>
2022-12-18 16:40:09 +0900

    Update brew.sh

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd 'dirname $0'
+
 echo 'install homebrew'
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -7,4 +9,4 @@ echo 'brew doctor, update, upgrade'
 brew doctor && brew cleanup && brew update --verbose && brew upgrade --verbose
 
 echo 'install app from brewfile'
-brew bundle --file ./config/Brewfile --verbose
+brew bundle --file ../config/Brewfile --verbose
