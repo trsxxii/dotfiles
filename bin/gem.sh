ccf0ae13ae5bab9160faa19598565866923a1c9f
Toru Asai <trsxxii@gmail.com>
2022-12-22 13:01:30 +0900

    Update gem.sh

--- bin/gem.sh
+++ bin/gem.sh
@@ -2,6 +2,12 @@
 
 cd $(dirname $0)
 
+echo 'setup rbenv'
+export PATH=$HOME/.rbenv/bin:$PATH
+rbenv install '2.6.8'
+rbenv global '2.6.8'
+eval "$(~/.rbenv/bin/rbenv init)"
+
 echo 'bundle install'
 gem install bundler
 bundle install --gemfile ../config/Gemfile
