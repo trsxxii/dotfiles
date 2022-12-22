ef34b027f5bbf72966c13eab8107ff2c867f32f4
Toru Asai <trsxxii@gmail.com>
2022-12-22 13:19:23 +0900

    Update gem.sh

--- bin/gem.sh
+++ bin/gem.sh
@@ -6,7 +6,7 @@ echo 'setup rbenv'
 export PATH=$HOME/.rbenv/bin:$PATH
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@3)"
 rbenv install '3.1.3'
-rbenv global '3.1.3'
+rbenv local '3.1.3'
 eval "$(~/.rbenv/bin/rbenv init)"
 
 echo 'bundle install'
