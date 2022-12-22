fee26ae22b256cb7e23512dba84237794fbee2df
Toru Asai <trsxxii@gmail.com>
2022-12-22 14:59:51 +0900

    Update gem.sh

--- bin/gem.sh
+++ bin/gem.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 cd $(dirname $0)
+source ~/.zshrc
 
 echo 'setup rbenv'
-export PATH=$HOME/.rbenv/bin:$PATH
 rbenv install '3.1.3'
 rbenv local '3.1.3'
-eval "$(~/.rbenv/bin/rbenv init)"
 
 echo 'bundle install'
-gem install bundler
+rbenv exec gem install bundler
 bundle install --gemfile ../config/Gemfile
